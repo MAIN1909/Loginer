3db8f11bf9a2e0289e133189839a0ddd557bb004
Khomenko Ivan <luckyvanya21@gmail.com>
2018-12-05 21:39:43 +0200

    init

---
+++ src/servlet/AuthServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AuthServlet", urlPatterns = "/")
+public class AuthServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String login = request.getParameter("login");
+        if ("admin".equals(login)) {
+            request.getSession().setAttribute("login", login);
+            response.sendRedirect("/loginer/main");
+        } else {
+            response.sendRedirect("/loginer/");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("<html>\n" +
+                    "  <head>\n" +
+                    "    <title></title>\n" +
+                    "  </head>\n" +
+                    "  <body>\n" +
+                    "    <div>\n" +
+                    "      <form action='/loginer/' method='POST'>\n" +
+                    "        <input type='text' name='login'/>\n" +
+                    "        <input type='submit' value='Sign In Me'/> \n" +
+                    "      </form>\n" +
+                    "    </div>\n" +
+                    "  </body>\n" +
+                    "</html>");
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/servlet/MainServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MainServlet", urlPatterns = "/main")
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (request.getSession().getAttribute("login") != null) {
+            response.setContentType("text/html");
+            response.setCharacterEncoding("UTF-8");
+            try {
+                PrintWriter out = response.getWriter();
+                out.println("Hello world!!! привет мир");
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            response.sendRedirect("/loginer/");
+        }
+    }
+}
