bb75f3ccde58e03f3607352dc26d5120a07abab9
MAIN1909 <main1909@ex.ua>
2018-12-19 19:02:02 +0200

    space

--- src/dao/FileSystemItemDao.java
+++ src/dao/FileSystemItemDao.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class FileSystemItemDao implements ItemDao {
+
     @Override
     public List<Item> get() {
         File id = new File("C:\\Users\\USER\\IdeaProjects\\Loginer2\\id.dat");
