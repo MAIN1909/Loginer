aedb7c3e3e367e38d949b92dde2e9e045f244c55
driveall <alexeyreznik83@gmail.com>
2019-01-30 19:54:54 +0200

    fixes

--- src/main/java/servlet/ItemServlet.java
+++ src/main/java/servlet/ItemServlet.java
@@ -5,14 +5,13 @@ import entity.Item;
 import service.ItemServise;
 import spring.SpringContextHolder;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ItemServlet", urlPatterns = "/item")
+//@WebServlet(name = "ItemServlet", urlPatterns = "/item")
 public class ItemServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
